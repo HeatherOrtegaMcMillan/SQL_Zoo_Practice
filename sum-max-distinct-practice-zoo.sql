708229e99c250317d84abda03dd995c7e09c0cff
Heather McMillan <heather.o.mcmillan@gmail.com>
2021-03-26 19:07:46 -0500

    update sum max distinct practice

--- sum-max-distinct-practice-zoo.sql
+++ sum-max-distinct-practice-zoo.sql
@@ -47,3 +47,31 @@ SELECT COUNT(winner) FROM nobel;
 -- 2. List each subject - just once
 SELECT DISTINCT subject
 FROM nobel;
+
+-- 3. Show the total number of prizes awarded for Physics.
+SELECT COUNT(subject)
+FROM nobel
+WHERE subject = 'physics';
+
+-- 4. For each subject show the subject and the number of prizes.
+SELECT subject, COUNT(subject)
+FROM nobel
+GROUP BY subject;
+
+-- 5. For each subject show the first year that the prize was awarded.
+SELECT subject, MIN(yr)
+FROM nobel
+GROUP BY subject;
+
+-- 6. For each subject show the number of prizes awarded in the year 2000.
+SELECT subject, count(subject)
+FROM nobel
+WHERE yr = 2000
+GROUP BY subject;
+
+-- 7. Show the number of different winners for each subject.
+SELECT subject, COUNT(DISTINCT winner)
+FROM nobel
+GROUP BY subject;
+
+-- 8. 
